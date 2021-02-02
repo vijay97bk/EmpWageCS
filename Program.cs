ccb8889372e1e41bce618ce6c380c21ec286b112
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-02 15:24:27 +0530

    [vijay]CheckEmpPresence

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Emp Wage");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int Emp_Check = random.Next(0, 2);
+            if (Emp_Check == IS_FULL_TIME) 
+            {
+                Console.WriteLine("Emp is present");
+            }
+            else { Console.WriteLine("Emp is Absent"); }
+        }
+
+    }
+
+}
