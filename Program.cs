2ebfe325e868e240966ff3410fcd7fec8dad47cc
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-02 15:49:58 +0530

    [vijay]PartTimeAdded

--- Program.cs
+++ Program.cs
@@ -7,19 +7,24 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Emp Wage");
-            int IS_FULL_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int IS_PART_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHours = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int Emp_Check = random.Next(0, 2);
-            if (Emp_Check == IS_FULL_TIME)
+            int Emp_Check = random.Next(0, 3);
+            if (Emp_Check == IS_PART_TIME)
+            {
+                empHours = 4;
+            }
+            else if (Emp_Check == IS_FULL_TIME) 
             {
                 empHours = 8;
-            }
-            else
+            }             
+           else
             {
                 empHours = 0;
             }
