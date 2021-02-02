4de8b0b7c51cb1d7b495eae18325da34f470d53c
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-02 15:38:03 +0530

    [vijay]DailyWageCalculation

--- Program.cs
+++ Program.cs
@@ -8,13 +8,23 @@ namespace EmpWage
         {
             Console.WriteLine("welcome to Emp Wage");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHours = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int Emp_Check = random.Next(0, 2);
-            if (Emp_Check == IS_FULL_TIME) 
+            if (Emp_Check == IS_FULL_TIME)
             {
-                Console.WriteLine("Emp is present");
+                empHours = 8;
             }
-            else { Console.WriteLine("Emp is Absent"); }
+            else
+            {
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine($"employee wage is {empWage}");
         }
 
     }
