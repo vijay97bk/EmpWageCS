b92099886e4834d8fcb3236980af58c1b9f89fcb
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-02 12:46:46 +0530

    [vijay]WelcomeMessage

---
+++ EmpWage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class EmpWage
+{
+	public void WelcomeMessage()
+	{
+		Console.WriteLine("Welcome to EmpWage Program");
+	}
+	static void main()
+    {
+		WelcomeMessage();
+		
+    }
+}
